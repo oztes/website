b0678a395edd5a0f78e15e4f09dfc2cfb9b98e3c
TeoS <ots1@williams..edu>
2023-12-04 01:15:14 -0500

    the thing

--- something.js
+++ something.js
@@ -1,22 +1,29 @@
-function randomColor() {
-    return '#' + Math.floor(Math.random()*16777215).toString(16);
-}
-
-function createGradient() {
-    const color1 = randomColor();
-    const color2 = randomColor();
-    const color3 = randomColor();
-    return `linear-gradient(${color1}, ${color2}, ${color3})`;
-}
-
-function populateGrid() {
-    const grid = document.getElementById('gradient-grid');
-    for (let i = 0; i < 9; i++) {
-        const cell = document.createElement('div');
-        cell.classList.add('grid-cell');
-        cell.style.background = createGradient();
-        grid.appendChild(cell);
+(function() {
+    function randomColor() {
+        return '#' + Math.floor(Math.random() * 16777215).toString(16);
     }
-}
 
-document.addEventListener('DOMContentLoaded', populateGrid);
+    function createGradient() {
+        const color1 = randomColor();
+        const color2 = randomColor();
+        const color3 = randomColor();
+        return `linear-gradient(${color1}, ${color2}, ${color3})`;
+    }
+
+    function populateGrid(rows, columns) {
+        const grid = document.getElementById('gradient-grid');
+        grid.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+        grid.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+
+        for (let i = 0; i < rows * columns; i++) {
+            const cell = document.createElement('div');
+            cell.classList.add('grid-cell');
+            cell.style.background = createGradient();
+            grid.appendChild(cell);
+        }
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        populateGrid(9, 3); // Set the number of rows and columns here
+    });
+})();
