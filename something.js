998e0c260b80d20853e2088dc6ee25cbf2ea0820
TeoS <ots1@williams..edu>
2023-12-05 01:35:17 -0500

    rowsto1

--- something.js
+++ something.js
@@ -5,23 +5,22 @@
 
     function populateGrid() {
         const grid = document.getElementById('color-grid');
-        const squareSize = 1; // Size of each square (width and height) in pixels
+        const squareSize = 10; // Size of each square (width and height) in pixels
 
         // Calculate number of columns and rows needed to fill the viewport
         const cols = Math.floor(window.innerWidth / squareSize);
-        const rows = Math.floor(window.innerHeight / squareSize);
+        const rows = 1;
 
         grid.style.display = 'grid';
         grid.style.gridTemplateColumns = `repeat(${cols}, ${squareSize}px)`;
 
-        for (let i = 0; i < rows; i++) {
-            for (let j = 0; j < cols; j++) {
+        
+        for (let j = 0; j < cols; j++) {
                 const cell = document.createElement('div');
                 cell.style.backgroundColor = randomColor();
                 cell.style.width = `${squareSize}px`;
                 cell.style.height = `${squareSize}px`;
                 grid.appendChild(cell);
-            }
         }
     }
 
